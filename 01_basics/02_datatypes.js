da61177498a3cb1136f0195adc709d995033e7b1
Aadish Jain <57306998+Aadish-jain@users.noreply.github.com>
2024-03-21 07:24:18 +0000

    learning datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -5,7 +5,12 @@ console.log(1 + 2);
 let name = "Aadish"
 let id = 12345
 let a = true
+// null
+// undefined
+// string = ""
+//bigint
+let bigint = 100000000
 
-console.log(typeof name);
 console.log(typeof null);
-
+console.log(typeof 100000999999999999900000000);
+console.log(typeof "Aadish jain");
