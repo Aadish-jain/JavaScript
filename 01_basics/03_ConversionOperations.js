f5358a87bcb6bfaa2d44e3f5a5b684c7f98b484b
Aadish Jain <57306998+Aadish-jain@users.noreply.github.com>
2024-03-27 06:57:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,24 @@
+let score = "33aa"
+
+// console.log(score);
+// console.log(typeof score);
+
+let variableNumber = Number(score)
+// console.log(variableNumber);
+// console.log(typeof variableNumber)
+
+let varString = String(score)
+// console.log(varString);
+// console.log(typeof varString)
+
+let newVar = '0'
+console.log(newVar)
+console.log(typeof newVar)
+
+let VarBool = Boolean(newVar)
+console.log(VarBool)
+console.log(typeof VarBool)
+
+//'aa' => 1, true
+//'' => 1, true
+//0 => false
