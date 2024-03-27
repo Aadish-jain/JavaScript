4f9e640aa1e643557b3a709d5a4a34aae6434f13
Aadish Jain <57306998+Aadish-jain@users.noreply.github.com>
2024-03-27 06:31:48 +0000

    variables and datatypes

---
+++ 01_basics/revise.js
@@ -0,0 +1,24 @@
+//VARIABLES
+/*
+const name = "Aadish Jain"
+let email = "aadish@github.com"
+//var email2 = "aadish@google.com"    // we can't use var because of scope issue, var don't know where the scope ends {....}
+password = 1234      // we can use any variable without declare keywords
+let aj;    //semicolon is optional in javascript
+
+console.table([name, email, password, aj])
+*/
+
+//DATA TYPES
+"use strict";    // treat all JS code as newer version
+// alert("Hello")   // use in browser not in node js
+
+let int = 10
+let bigint = 111111111111111112222222222222222222222222222222222222
+let bool = true
+let str = "aadish"
+let nl = null
+let un = undefined
+console.table([int, bigint, bool, str, nl, un])
+console.table([typeof int, typeof bigint, typeof bool, typeof str, typeof nl, typeof un])
+
