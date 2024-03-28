63fb3bf6183426b96b4c1dc107ff8abcf26b3038
Aadish Jain <57306998+Aadish-jain@users.noreply.github.com>
2024-03-28 05:37:02 +0000

    Operations

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -12,13 +12,51 @@ let varString = String(score)
 // console.log(typeof varString)
 
 let newVar = '0'
-console.log(newVar)
-console.log(typeof newVar)
+// console.log(newVar)
+// console.log(typeof newVar)
 
 let VarBool = Boolean(newVar)
-console.log(VarBool)
-console.log(typeof VarBool)
+// console.log(VarBool)
+// console.log(typeof VarBool)
 
 //'aa' => 1, true
 //'' => 1, true
 //0 => false
+
+// Operations:
+
+// console.log(1+1)                //2
+// console.log("1" + 1)            //11
+// console.log(1 + 2 + "2")        //32
+// console.log("aadish" + 2)       //aadish2
+let value = 10
+let newValue = -value           //-10
+// console.log(value)              //10
+// console.log(newValue)           //-10
+
+let str1 = "hello"
+let str2 = " aadish"
+let str3 = str1 + str2         
+// console.log(str3)              // hello aadish
+
+// Tricky code
+console.log(true)
+console.log(+true)
+console.log("")
+console.log(+"")    //convert into number
+console.log("s")
+console.log(+"s")  // Not a Number NaN
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 1 + 4    // tricky (not recommended - code readablitiy is required)
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+
+
+
+
+
+
