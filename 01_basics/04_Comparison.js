ba45186e26adfa77328d9a86b55f140194b1b0f0
Aadish Jain <57306998+Aadish-jain@users.noreply.github.com>
2024-03-28 06:00:16 +0000

    Comparison

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,32 @@
+// console.log(2 == 2)       //true
+// console.log(2 > 1)        //true
+// console.log(2 >= 1)       //true
+// console.log(2 < 1)        //false
+// console.log(2 <= 1)       //false
+// console.log("2" == 1)     //false
+// console.log("2" > 1)      //true
+// console.log("2" < 1)      //false
+// console.log("2" <= 1)     //false
+// console.log("2" >= 1)     //true
+// console.log(2 != 0)       //true
+
+
+
+//strict check using === operator  : I that case conversion not happen, only comparison will happen
+console.log("2" === 2)        //false "2" -> is of datatype string and 2 is of datatype number
+
+
+// console.log(null == 0)
+// console.log(null < 0)
+// console.log(null <= 0)
+// console.log(null > 0)
+// console.log(null >= 0)
+
+
+console.log(undefined == 0)
+console.log(undefined != 0)
+console.log(undefined === 0)
+console.log(undefined < 0)
+console.log(undefined <= 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
